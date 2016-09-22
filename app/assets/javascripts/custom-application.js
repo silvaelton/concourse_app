2d52f7f1b4e0cacd991457197b96ef848af2dfb2
elton silva <elton.chrls@gmail.com>
2016-09-22 14:07:55 -0300

    ok

--- app/assets/javascripts/custom-application.js
+++ app/assets/javascripts/custom-application.js
@@ -6,10 +6,27 @@ $(document).ready(function() {
   $('.hour').mask("00:00", {placeholder: "08:00"});
   $('.cnpj').mask("00.000.000/0000-00", {placeholder: "00.000.000/0000-00"});
 
-  tinyMCE.init({
-    selector: 'textarea.tinymce',
-    language: 'pt_BR',
-    plugins : 'advlist autolink link uploadimage image lists charmap print preview'
-  });
+  tinymce.init({
+    selector: 'textarea',
+    height: 500,
+    theme: 'modern',
+    plugins: [
+      'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+      'searchreplace wordcount visualblocks visualchars code fullscreen',
+      'insertdatetime media nonbreaking save table contextmenu directionality',
+      'emoticons template paste textcolor colorpicker textpattern imagetools'
+    ],
+    toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+    toolbar2: 'print preview media | forecolor backcolor emoticons',
+    image_advtab: true,
+    templates: [
+      { title: 'Test template 1', content: 'Test 1' },
+      { title: 'Test template 2', content: 'Test 2' }
+    ],
+    content_css: [
+      '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+      '//www.tinymce.com/css/codepen.min.css'
+    ]
+   });
 });
 
