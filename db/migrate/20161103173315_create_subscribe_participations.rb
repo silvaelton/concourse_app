613082150f3dc0e400251fdb446385d58c15cb06
Elton Silva <elton.chrls@gmail.com>
2016-11-03 16:35:15 -0200

    ok

--- db/migrate/20161103173315_create_subscribe_participations.rb
+++ db/migrate/20161103173315_create_subscribe_participations.rb
@@ -4,6 +4,7 @@ class CreateSubscribeParticipations < ActiveRecord::Migration
 
       t.integer :subscribe_id, index: true 
       t.integer :participation_id, index: true 
+      t.string  :participation_file
       
       t.timestamps null: false
     end
