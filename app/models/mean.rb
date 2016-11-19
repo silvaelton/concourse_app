337f45887b30acf46156c49f148993e1170e7c18
Elton Silva! <elton.chrls@gmail.com>
2016-11-19 18:01:42 -0200

    ok

--- app/models/mean.rb
+++ app/models/mean.rb
@@ -12,7 +12,12 @@ class Mean < ActiveRecord::Base
   private
 
   def subscribe_is_valid?
-    if !::Subscribe.where(id: self.subscribe_id, password: self.password).present?
+    if ::Subscribe.where(id: self.subscribe_id, password: self.password).present?
+      participation = ::Participation.find_by(name: "Prancha A1 (PDF até 15mb)") rescue nil
+      if !::SubscribseParticipation.where(participation_id: participation.id, subscribe_id: self.subscribe_id).present?
+        errors.add(:subscribe_id, "Nº de inscrição não realizou envio de participação para este concurso")
+      end
+    else
       errors.add(:subscribe_id, "Nº de inscrição ou senha inválidos")
     end
   end
