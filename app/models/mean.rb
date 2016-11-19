fc89031b6265bf8d1ffd9ff6ef09b6b6eee7db63
Elton Silva! <elton.chrls@gmail.com>
2016-11-19 18:02:37 -0200

    ok

--- app/models/mean.rb
+++ app/models/mean.rb
@@ -4,7 +4,7 @@ class Mean < ActiveRecord::Base
 	
   attr_accessor :password
 
-  validates :content, presence: true
+  validates :content, :subscribe_id, :password, presence: true
   validates_uniqueness_of :subscribe_id, scope: :project_id 
   validate :subscribe_is_valid?
 
