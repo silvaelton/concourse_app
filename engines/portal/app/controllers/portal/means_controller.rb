68aae1f0c30b6c38ce9ab9dbbf359f467c00cdf5
Elton Silva! <elton.chrls@gmail.com>
2016-11-25 05:47:02 -0200

    set_authenticate

--- engines/portal/app/controllers/portal/means_controller.rb
+++ engines/portal/app/controllers/portal/means_controller.rb
@@ -2,7 +2,8 @@ require_dependency 'portal/application_controller'
 
 module Portal
 	class MeansController < ApplicationController 
-		before_action :set_project
+    before_action :set_project
+		before_action :set_authenticate
 
     layout 'sub_portal'
     
@@ -38,5 +39,11 @@ module Portal
 		def set_project
 			@project = ::Project.friendly.find(params[:project_id])
 		end
+
+    def set_authenticate
+      @user = ::User.find(session[:admin_id]) rescue nil
+      
+      redirect_to '/404' @user.nil?
+    end
 	end
 end
