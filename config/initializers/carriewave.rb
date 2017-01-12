2e27e06abedf2bcd6533c3c78d67f821236d6a5b
Elton Silva <elton.chrls@gmail.com>
2017-01-12 09:11:26 -0200

    fix amaxon

--- config/initializers/carriewave.rb
+++ config/initializers/carriewave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:      ENV['S3_KEY'],                        # required
     aws_secret_access_key:  ENV['S3_SECRET'],                        # required
-    region:                'us-east-1',                  # optional, defaults to 'us-east-1'
+    region:                'us-west-1',                  # optional, defaults to 'us-east-1'
   }
 
   config.fog_directory  = 'concourseapp'                          # required
