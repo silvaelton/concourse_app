381d2808cc711080e5dd07c3abf7bae5b635d68f
Elton Silva <elton.chrls@gmail.com>
2017-01-12 09:08:37 -0200

    ok

--- Gemfile
+++ Gemfile
@@ -44,6 +44,10 @@ group :development, :test do
 
 end
 
+group :production do 
+  gem 'rails_12factor'
+end
+
 
 path 'engines' do 
   gem 'portal'
---
+++ engines/portal/app/controllers/portal/candidate_area/pay_documents_controller.rb
@@ -0,0 +1,32 @@
+require_dependency 'portal/application_controller'
+
+module Portal
+  module CandidateArea
+    class PayDocumentsController < ApplicationController
+      before_action :set_project
+      before_action :subscribe_authenticate!
+        
+      layout 'sub_portal'
+
+      def edit
+        @subscribe = current_candidate
+      end
+
+      def update
+        @subscribe = current_candidate
+        @subscribe.update(set_params)
+      end
+
+      private
+
+      def set_project
+        @project = ::Project.friendly.find(params[:project_id])
+      end 
+
+      def set_params
+        params.fetch(:subscribe, {}).permit(:paid_document)
+      end
+   
+    end
+  end
+end
