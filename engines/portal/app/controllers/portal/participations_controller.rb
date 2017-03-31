b8ab57b6f5d9e0708bc110b9af34cf84ae6bbfe0
Elton Silva <elton.chrls@gmail.com>
2017-03-31 14:24:48 -0300

    Fix page

--- engines/portal/app/controllers/portal/participations_controller.rb
+++ engines/portal/app/controllers/portal/participations_controller.rb
@@ -8,7 +8,8 @@ module Portal
 
     def all
       if @project.id == 3
-        @projects = ::SubscribeCustomParticipation.all.order(:id)
+        participation = ::Participation.find_by(name: "Prancha A1 (PDF até 15mb)") rescue nil
+        @projects = ::SubscribeParticipation.where(participation_id: participation.id)
       end
 
       if @project.id == 1
